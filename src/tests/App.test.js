185087c671825795e156bb2a457ec825271a94df
Angelica <angelicapedroso05@gmail.com>
2021-12-01 16:26:07 -0300

    requirement one:tests if the app component contains a set of navigation links

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../services/renderWithRouter';
+import App from '../App';
+
+describe('Test the <App.js /> component', () => {
+  test('The application must have a fixed set of navigation links', () => {
+    renderWithRouter(<App />);
+
+    const home = screen.getByRole('link', { name: /home/i });
+    expect(home).toBeInTheDocument();
+
+    const about = screen.getByRole('link', { name: /about/i });
+    expect(about).toBeInTheDocument();
+
+    const favoritePokemons = screen.getByRole('link', { name: /favorite pokémons/i });
+    expect(favoritePokemons).toBeInTheDocument();
+  });
+});
