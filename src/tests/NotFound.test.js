bfbea9a3f973aababa5584f3f6908bf81de222ed
Angelica <angelicapedroso05@gmail.com>
2021-12-03 18:22:28 -0300

    test(NotFound):test the text and test the image

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import NotFound from '../components/NotFound';
+
+describe('test the <NotFound.js /> component', () => {
+  test('test if the image contain a heading h2 and test the text', () => {
+    render(<NotFound />);
+    const title = screen.getByRole('heading', {
+      name: /Page requested not found/i,
+      level: 2,
+    });
+    expect(title).toBeInTheDocument();
+  });
+
+  test('check the image', () => {
+    render(<NotFound />);
+    const imagem = screen.getByAltText(
+      'Pikachu crying because the page requested was not found',
+    );
+    expect(imagem).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
