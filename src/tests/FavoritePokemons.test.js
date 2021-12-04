a242e102f035724709f8da8eb3a70b98ef33b82d
Angelica <angelicapedroso05@gmail.com>
2021-12-03 21:44:17 -0300

    off(FavoritePokemons):add collaboration

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,3 +1,4 @@
+//segunda parte foi feita com ajuda do Pedro Lima 
 import { render, screen } from '@testing-library/react';
 import React from 'react';
 import FavoritePokemons from '../components/FavoritePokemons';
